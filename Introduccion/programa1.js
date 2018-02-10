3494aa83c5d8c46896476b79837744d990768637
Erik Rodriguez <rodriguezestuardo.03@gmail.com>
2018-02-10 10:09:15 -0600

    it is the first file

---
+++ Introduccion/programa1.js
@@ -0,0 +1,15 @@
+var nombre = "Erik Estuardo Rodriguez Guillen";
+var carnet = "17002142";
+var mes = 10;
+if (mes == 12 || mes == 1 || mes == 2) {
+  alert("es invierno");
+} else if (mes == 3 || mes == 4 || mes == 5){
+  alert("es primavera");
+} else if (mes == 6 || mes == 7 || mes == 8) {
+  alert("es verano");
+}else if (mes == 9 || mes == 10 || mes == 11) {
+  alert("es otoño");
+}else { alert("Numero no encontrado");}
+
+alert(carnet);
+alert(nombre);
